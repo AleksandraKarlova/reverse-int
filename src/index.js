8e43b6f8cbe450a9d3f489cf60c54cfc0fb1f506
Aleksandra Karlova <aleksandra.karlova10@gmail.com>
2024-08-18 14:35:48 +0200

    feat: add the soultion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
   
+  let arrNumber = n.toString().split('');
+
+  let arrNewNumber = arrNumber.reverse();
+
+  if (arrNewNumber.at(-1) === '-') {
+    arrNewNumber.pop();
+  }
+
+  return Number(arrNewNumber.join(''));
 }
